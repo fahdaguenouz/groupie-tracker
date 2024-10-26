2f5c3f39f12b9faa570860fc56d36ccc93bfb1d4
Fahd Aguenouz <fahd>
2024-10-26 15:37:31 +0100

    adding the error style

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 
 	http.HandleFunc("/",handlers.MainHandler)
 	http.HandleFunc("/artist/{id}",handlers.MainHandler)
+	http.HandleFunc("/assets/", handlers.AssetsHandler)
 
 	fmt.Println("Server is running at http://localhost:3000")
 	err:=http.ListenAndServe(":3000",nil)
