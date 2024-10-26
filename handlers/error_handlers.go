8a8248bc70dd3722718b54e71a411b344a1e28a3
Fahd Aguenouz <fahd>
2024-10-26 15:48:19 +0100

    structing the project

--- handlers/error_handlers.go
+++ handlers/error_handlers.go
@@ -1,29 +1,25 @@
 package handlers
 
 import (
+	"groupie/models"
 	"html/template"
 	"net/http"
 )
 
-type Error struct {
-	StatusCode int
-	Error      string
-}
-
-func ErrorHandler(w http.ResponseWriter, r *http.Request,statusCode int) {
-	template,err:=template.ParseFiles("templates/error.html")
-	if err!=nil{
-		http.Error(w,err.Error(),http.StatusInternalServerError)
-        return
+func ErrorHandler(w http.ResponseWriter, r *http.Request, statusCode int) {
+	template, err := template.ParseFiles("templates/error.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	customError:=Error{
+	customError := models.Error{
 		StatusCode: http.StatusNotFound,
-        Error:      http.StatusText(statusCode),
+		Error:      http.StatusText(statusCode),
 	}
 	w.WriteHeader(statusCode)
-	if err:=template.Execute(w,customError);err!=nil{
-		http.Error(w,err.Error(),http.StatusInternalServerError)
-        return
+	if err := template.Execute(w, customError); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 }
